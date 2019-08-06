4559ea46ec34063cde1305fc01ce0082a23048c3
haroonqaiser <haroonq.it@hotmail.com>
2019-08-06 23:18:10 +0500

    db and other basic working

---
+++ AcBazar.Database/ACContext.cs
@@ -0,0 +1,21 @@
+﻿using AcBazar.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcBazar.Database
+{
+    public class ACContext:DbContext
+    {
+        public ACContext() : base("ACBazarConnection")
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
